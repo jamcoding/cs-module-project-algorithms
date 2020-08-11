04928a1f41a65ada7155d9c9e3e7481fbc6650f1
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-08-11 17:12:26 -0500

    finished moving_zeroes.py

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,14 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    count = 0
 
-    pass
+    for i in range(0, len(arr)):
+        if(arr[i] != 0):
+            arr[count], arr[i] = arr[i], arr[count]
+            count += 1
+    print(arr)
+    return arr
 
 
 if __name__ == '__main__':
